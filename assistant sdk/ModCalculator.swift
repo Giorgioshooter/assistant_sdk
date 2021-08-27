e7c920266645f619c3c68d5a7a0901e1c4c84045
Georgios Galatoulas <georgiosgalatoulas@gmail.com>
2021-08-27 11:27:29 +0100

    Initial commit

---
+++ assistant sdk/ModCalculator.swift
@@ -0,0 +1,14 @@
+//
+//  ModCalculator.swift
+//  assistant sdk
+//
+//  Created by Georgios Galatoulas on 27/08/2021.
+//
+
+import Foundation
+
+public class ModCalculator {
+    public func mod(of num:Int, with mod:Int)-> Int {
+        return num % mod
+    }
+}
--- assistant sdkTests/assistant_sdkTests.swift
+++ assistant sdkTests/assistant_sdkTests.swift
@@ -10,23 +10,16 @@ import XCTest
 
 class assistant_sdkTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testMod() throws {
+        let calc = ModCalculator()
+        XCTAssertEqual(calc.mod(of: 9, with: 4), 1, "Wrong number")
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
+    func testModPerformanceExample() throws {
         // This is an example of a performance test case.
         self.measure {
-            // Put the code you want to measure the time of here.
+            let calc = ModCalculator()
+            XCTAssertEqual(calc.mod(of: 9, with: 4), 1, "Wrong number")
         }
     }
 
