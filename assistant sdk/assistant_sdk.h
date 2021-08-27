fba0947d5c467c506d556b2d08df7b5f95c8e004
Georgios Galatoulas <georgiosgalatoulas@gmail.com>
2021-08-27 11:14:58 +0100

    Initial Commit

---
+++ assistant sdk/assistant_sdk.h
@@ -0,0 +1,18 @@
+//
+//  assistant_sdk.h
+//  assistant sdk
+//
+//  Created by Georgios Galatoulas on 27/08/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for assistant_sdk.
+FOUNDATION_EXPORT double assistant_sdkVersionNumber;
+
+//! Project version string for assistant_sdk.
+FOUNDATION_EXPORT const unsigned char assistant_sdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <assistant_sdk/PublicHeader.h>
+
+
